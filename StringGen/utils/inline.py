0a717838d8fae84623b4fa51e442dc7836171426
Yatoogod <email.yamann@gmail.com>
2024-11-11 01:06:15 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="Generate session", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(text="Support", url=https://t.me/Yakuza_Units),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="Owner", url="https://github.com/AnonymousX1025/StringGenBot"
             ),
         ],
     ]
